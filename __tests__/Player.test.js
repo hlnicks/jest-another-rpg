815f7f55a080c634102ce4d52e26cb4e401abf3c
hlnicks <hlnicks@icloud.com>
2022-07-02 13:06:22 -0400

    created player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,7 +4,7 @@ const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
 
 test('creates a player object', () => {
-    const player = new Player ('Dave');
+    const player = new Player('Dave');
 
     expect(player.name).toBe('Dave');
     expect(player.health).toEqual(expect.any(Number));
@@ -30,4 +30,32 @@ test('gets inventory from player or returns false', () => {
     player.inventory = [];
 
     expect(player.getInventory()).toEqual(false);
+});
+
+test('gets players health value', () => {
+    const player = new Player('Dave');
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test('subtracts from players health', () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
 });
